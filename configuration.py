1a79aedaf0c6a2b050eea793fb47a27aac515e64
Doino Gretchenliev <doino.gretchenliev@kubit.co>
2025-11-03 20:40:58 +0200

    added support for ntp server

--- configuration.py
+++ configuration.py
@@ -1,4 +1,27 @@
-from constants import CONFIGURATION_FILE, CONFIGURATION_RUN_BLINK_TIME_COLON, CONFIGURATION_RUN_CLOCK_TYPE, CONFIGURATION_RUN_AUTOLIGHT, CONFIGURATION_RUN_SHOW_TEMP, CONFIGURATION_MQTT_BROKER, CONFIGURATION_MQTT_CONFIG, CONFIGURATION_MQTT_ENABLED, CONFIGURATION_MQTT_PREFIX, CONFIGURATION_MQTT_USERNAME, CONFIGURATION_MQTT_PASSWORD, CONFIGURATION_RUN_CONFIG, CONFIGURATION_RUN_TEMP, CONFIGURATION_WIFI_CONFIG, CONFIGURATION_WIFI_ENABLED, CONFIGURATION_WIFI_HOSTNAME, CONFIGURATION_WIFI_SSID, CONFIGURATION_WIFI_PASSPHRASE, CONFIGURATION_WIFI_NTP_ENABLED, CONFIGURATION_WIFI_NTP_PTZ
+from constants import (
+    CONFIGURATION_FILE,
+    CONFIGURATION_RUN_BLINK_TIME_COLON,
+    CONFIGURATION_RUN_CLOCK_TYPE,
+    CONFIGURATION_RUN_AUTOLIGHT,
+    CONFIGURATION_RUN_SHOW_TEMP,
+    CONFIGURATION_MQTT_BROKER,
+    CONFIGURATION_MQTT_CONFIG,
+    CONFIGURATION_MQTT_ENABLED,
+    CONFIGURATION_MQTT_PREFIX,
+    CONFIGURATION_MQTT_USERNAME,
+    CONFIGURATION_MQTT_PASSWORD,
+    CONFIGURATION_RUN_CONFIG,
+    CONFIGURATION_RUN_TEMP,
+    CONFIGURATION_WIFI_CONFIG,
+    CONFIGURATION_WIFI_ENABLED,
+    CONFIGURATION_WIFI_HOSTNAME,
+    CONFIGURATION_WIFI_SSID,
+    CONFIGURATION_WIFI_PASSPHRASE,
+    CONFIGURATION_NTP_CONFIG,
+    CONFIGURATION_NTP_ENABLED,
+    CONFIGURATION_NTP_SERVER,
+    CONFIGURATION_NTP_INTERVAL,
+)
 from util import singleton
 from helpers import read_json_file, write_json_file
 
@@ -6,13 +29,11 @@ from helpers import read_json_file, write_json_file
 @singleton
 class Configuration:
     class WifiConfiguration:
-        def __init__(self, enabled: bool, hostname: str, ssid: str, passphrase: str, ntp_enabled: bool, ntp_ptz: str) -> None:
+        def __init__(self, enabled: bool, hostname: str, ssid: str, passphrase: str) -> None:
             self.enabled = enabled
             self.hostname = hostname
             self.ssid = ssid
             self.passphrase = passphrase
-            self.ntp_enabled = ntp_enabled
-            self.ntp_ptz = ntp_ptz
 
     class MQTTConfiguration:
         def __init__(self, enabled: bool, broker: str, prefix: str, username: str, password: str) -> None:
@@ -22,6 +43,12 @@ class Configuration:
             self.base_topic = prefix + "/"
             self.username = username
             self.password = password
+
+    class NTPConfiguration:
+        def __init__(self, enabled: bool, server: str, interval: int) -> None:
+            self.enabled = enabled
+            self.server = server
+            self.interval = interval
 
     def __init__(self) -> None:
         self.config = {}
@@ -37,47 +64,51 @@ class Configuration:
         self.update_config_variables()
 
     def update_config_variables(self):
+        # runConfig
         self.blink_time_colon = self.config[CONFIGURATION_RUN_CONFIG][CONFIGURATION_RUN_BLINK_TIME_COLON]
         self.temp = self.config[CONFIGURATION_RUN_CONFIG][CONFIGURATION_RUN_TEMP]
         self.clock_type = self.config[CONFIGURATION_RUN_CONFIG][CONFIGURATION_RUN_CLOCK_TYPE]
         self.show_temp = self.config[CONFIGURATION_RUN_CONFIG][CONFIGURATION_RUN_SHOW_TEMP]
         self.autolight = self.config[CONFIGURATION_RUN_CONFIG][CONFIGURATION_RUN_AUTOLIGHT]
 
+        # wifiConfig
         self.wifi_config = self.WifiConfiguration(
             enabled=self.config[CONFIGURATION_WIFI_CONFIG][CONFIGURATION_WIFI_ENABLED],
             hostname=self.config[CONFIGURATION_WIFI_CONFIG][CONFIGURATION_WIFI_HOSTNAME],
             ssid=self.config[CONFIGURATION_WIFI_CONFIG][CONFIGURATION_WIFI_SSID],
             passphrase=self.config[CONFIGURATION_WIFI_CONFIG][CONFIGURATION_WIFI_PASSPHRASE],
-            ntp_enabled=self.config[CONFIGURATION_WIFI_CONFIG][CONFIGURATION_WIFI_NTP_ENABLED],
-            ntp_ptz=self.config[CONFIGURATION_WIFI_CONFIG][CONFIGURATION_WIFI_NTP_PTZ]
         )
 
+        # mqttConfig
         self.mqtt_config = self.MQTTConfiguration(
             enabled=self.config[CONFIGURATION_MQTT_CONFIG][CONFIGURATION_MQTT_ENABLED],
             broker=self.config[CONFIGURATION_MQTT_CONFIG][CONFIGURATION_MQTT_BROKER],
             prefix=self.config[CONFIGURATION_MQTT_CONFIG][CONFIGURATION_MQTT_PREFIX],
             username=self.config[CONFIGURATION_MQTT_CONFIG][CONFIGURATION_MQTT_USERNAME],
-            password=self.config[CONFIGURATION_MQTT_CONFIG][CONFIGURATION_MQTT_PASSWORD]
+            password=self.config[CONFIGURATION_MQTT_CONFIG][CONFIGURATION_MQTT_PASSWORD],
+        )
+
+        # ntpConfig
+        ntp_conf = self.config.get(CONFIGURATION_NTP_CONFIG, {})
+        self.ntp_config = self.NTPConfiguration(
+            enabled=ntp_conf.get(CONFIGURATION_NTP_ENABLED, True),
+            server=ntp_conf.get(CONFIGURATION_NTP_SERVER, "pool.ntp.org"),
+            interval=ntp_conf.get(CONFIGURATION_NTP_INTERVAL, 21600),
         )
 
     def write_config_file(self):
         write_json_file(CONFIGURATION_FILE, self.config)
         self.update_config_variables()
 
+    # toggles/updates
     def switch_blink_time_colon_value(self):
-        if self.config[CONFIGURATION_RUN_CONFIG][CONFIGURATION_RUN_BLINK_TIME_COLON]:
-            self.config[CONFIGURATION_RUN_CONFIG][CONFIGURATION_RUN_BLINK_TIME_COLON] = False
-        else:
-            self.config[CONFIGURATION_RUN_CONFIG][CONFIGURATION_RUN_BLINK_TIME_COLON] = True
-
+        key = CONFIGURATION_RUN_BLINK_TIME_COLON
+        self.config[CONFIGURATION_RUN_CONFIG][key] = not self.config[CONFIGURATION_RUN_CONFIG][key]
         self.write_config_file()
 
     def switch_temp_value(self):
-        if self.config[CONFIGURATION_RUN_CONFIG][CONFIGURATION_RUN_TEMP] == "c":
-            self.config[CONFIGURATION_RUN_CONFIG][CONFIGURATION_RUN_TEMP] = "f"
-        else:
-            self.config[CONFIGURATION_RUN_CONFIG][CONFIGURATION_RUN_TEMP] = "c"
-
+        key = CONFIGURATION_RUN_TEMP
+        self.config[CONFIGURATION_RUN_CONFIG][key] = "f" if self.config[CONFIGURATION_RUN_CONFIG][key] == "c" else "c"
         self.write_config_file()
 
     def update_clock_type_value(self, value):
